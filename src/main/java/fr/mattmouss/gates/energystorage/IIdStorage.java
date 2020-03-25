2ee9c857b8ec2bfd86ddee43b70aa79e599fbf91
matt1999rd <matthieu.moussiegt@gmail.com>
2020-03-25 19:20:02 +0100

    correcting bug and turn stile is working not checked the key item id registering

--- src/main/java/fr/mattmouss/gates/energystorage/IIdStorage.java
+++
@@ -1,9 +0,0 @@
-package fr.mattmouss.gates.energystorage;
-
-public interface IIdStorage {
-    int getEnergyStored();
-    void changeId();
-    void changeId(int id);
-
-
-}
---
+++ src/main/java/fr/mattmouss/gates/items/TurnStileKeyItem.java
@@ -0,0 +1,4 @@
+package fr.mattmouss.gates.items;
+
+public class TurnStileKeyItem {
+}
